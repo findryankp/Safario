1017586db6a3c116358499739bae4cd0e8dc0352
Unknown <hudafauzanm@gmail.com>
2019-04-28 12:35:50 +0700

    update view mahasiswa

--- controller/mahasiswa.js
+++ controller/mahasiswa.js
@@ -1,13 +1,29 @@
+var db = require("../db_config");
 var express = require('express');
 var router = express.Router();
+var var_dump = require('var_dump')
 
 router.get('/', function(request, response) {
-    response.render('mahasiswa/index.njk');
+	var username = request.session.username;
+	var nama = request.session.nama; 
+    response.render('mahasiswa/index.njk',{username,nama});
 });
 
-// router.get('/createkelas', function(request, response) {
-//     response.render('dosen/createkelas.njk'); //akses halaman buat kelas
-//     //isine form
-// });
+router.post('/absen', function(request, response) {
+	let sql = "UPDATE absen SET status='2'";
+	let query = db.query(sql, (err, results) => {
+		if(err) throw err;
+		response.redirect('/mahasiswa.njk');
+	});
+});
+
+router.post('/kelasmahasiswa',function(request,response){
+	let sql = "SELECT * FROM matkul";
+	let query = db.query(sql, (err, results) => {
+		if(err) throw err;
+		response.render('mahasiswa/index.njk',{results : results});
+	});
+});
+
 
 module.exports = router;
